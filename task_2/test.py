0f7c7513295f9bc414351e1ff2659d529422ffe8
Wex <wex@192.168.5.14>
2018-03-28 17:23:58 +0200

    Counting passed tests instead of failed ones

--- task_2/test.py
+++ task_2/test.py
@@ -12,13 +12,15 @@ def main():
         ("[P(y)th[on i]s a.n e{a()sy} {[()]}(t[])o learn [lang]u{age}...]]", False)
     ]
 
-    n_failed = 0
+    n_passed = 0
     for idx, test in enumerate(TESTS):
         output = check_bracket_balance(test[0])
         if(test[1] != output):
-            n_failed += 1
-            print("Failed test %d.\nInput: %s\nExpected output: %s\nOutput: %s" % (idx, test[0], test[1], output))
-    print("Failed tests: %d / %d" % (n_failed, len(TESTS)))
+            print("Test %d FAILED\nInput: %s\nExpected output: %s\nOutput: %s\n" % (idx, test[0], test[1], output))
+        else:
+            n_passed += 1
+            print("Test %d PASSED" % idx)
+    print("Passed tests: %d / %d" % (n_passed, len(TESTS)))
 
 if __name__ == '__main__':
     main()
